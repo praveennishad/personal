8fd524ab3376acd34f76d33b8a12560d17321e32
praveennishad <45622087+praveennishad@users.noreply.github.com>
2018-12-05 14:30:01 +0530

    Add files via upload

---
+++ config/env/development.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: 'mongodb://localhost/meand',
+    sessionSecret: 'developmentSessionSecret',
+    facebook: {    clientID: 'FACEBOOK_APP_ID',    clientSecret: 'FACEBOOK_APP_SECRET',    callbackURL: 'http://localhost:3000/oauth/facebook/callback'  } 
+   };
